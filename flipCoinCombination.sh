a5afdd32cdaf7f3ec535f524fe081d7f8098435b
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-03-30 19:10:44 +0530

    Added : Percentage of Singlet Combination

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,14 +1,38 @@
 #!/bin/bash -x
 
+declare -A singletDict
+
+NO_TIMES_COIN_FLIP=10
 IS_HEAD=1
 
-bin=$((RANDOM%2))
+hCount=0
+tCount=0
+count=0
 
-if [ $bin -eq $IS_HEAD ]
-then
-        coin="heads"
-else
-        coin="tails"
-fi
+while [ $count -ne $NO_TIMES_COIN_FLIP  ]
+do
+	bin=$((RANDOM%2))
 
-echo "$coin"
+	singletDict[$count]=$bin
+
+	if [ $bin -eq $IS_HEAD ]
+	then
+        	coin="heads"
+        	hCount=$(($hCount+1))
+	else
+        	coin="tails"
+        	tCount=$(($tCount+1))
+	fi
+
+((count++))
+
+done
+
+
+hWinsPercent=`echo "scale=3;$hCount/$NO_TIMES_COIN_FLIP*100" | bc`
+tWinsPercent=`echo "scale=3;$tCount/$NO_TIMES_COIN_FLIP*100" | bc`
+
+echo ${singletDict[@]}
+echo "Heads Wins Percentage : "$hWinsPercent
+echo "Tails Wins Percentage : "$tWinsPercent
+
