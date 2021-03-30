a16b9388db4dbc2d5296f49de2b212ad7ea38bcb
VinayakKamat95 <omkarkamat1995@gmail.com>
2021-03-30 19:03:34 +0530

    Added : Flipping a Coin

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -1,1 +1,14 @@
-#welcome to Flip Coin Combination Problem
+#!/bin/bash -x
+
+IS_HEAD=1
+
+bin=$((RANDOM%2))
+
+if [ $bin -eq $IS_HEAD ]
+then
+        coin="heads"
+else
+        coin="tails"
+fi
+
+echo "$coin"
